ffb50310806493b96f75bd564990bad715026a74
niko-matses <nikolas.matses@gmail.com>
2020-06-24 17:49:25 -0400

    looping through arrays with v-for

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -4,6 +4,19 @@ var app = new Vue({
         product: 'Socks',
         description: 'These are the dryest socks and they keep your feet warm too',
         image: './assets/vmSocks-green-onWhite.jpg',
-        inventory: 8
+        inventory: 8,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        sizes: ["small", "medium", "large"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
